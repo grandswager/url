39e3dd40099bd56a5e2c7678a0736b84b8fdd57f
grandswager <96254408+grandswager@users.noreply.github.com>
2022-05-13 13:23:49 +0000

    Initial commit (2)

--- script.js
+++ script.js
@@ -11,9 +11,4 @@ function shorten(query, redirect) {
 
 function onload() {
   shorten("grandswager", "https://youtube.com/c/grandswager");
-  shorten("perutzism", "https://www.youtube.com/channel/UCWYsc6p0iE2lXsp_bP2P1Ng");
-  shorten("files", "https://filesharing.grandswager.repl.co");
-  shorten("bedwars", "https://bedwars.com");
-  shorten("islands", "https://web.roblox.com/games/4872321990/");
-  shorten("easygg", "https://easy.gg");
 }
