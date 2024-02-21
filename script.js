49051adc15600301b97aec5a9c1bed9eb508be87
grandswager <5575498-grandswager@users.noreply.replit.com>
2024-02-21 00:54:34 +0000

    Update queries

--- script.js
+++ script.js
@@ -11,4 +11,12 @@ function shorten(query, redirect) {
 
 function onload() {
   shorten("grandswager", "https://youtube.com/c/grandswager");
+  shorten("perutzism", "https://www.youtube.com/channel/UCWYsc6p0iE2lXsp_bP2P1Ng");
+  shorten("files", "https://filesharing.grandswager.repl.co");
+  shorten("bedwars", "https://bedwars.com");
+  shorten("islands", "https://web.roblox.com/games/4872321990/");
+  shorten("easygg", "https://easy.gg");
+  shorten("easy.gg", "https://easy.gg");
+  shorten("he", "https://hamburgeremporium.grandswager.repl.co/?ref-code=g-url");
+  shorten("perseus-slides", "https://docs.google.com/presentation/d/1DNDTSE3GU_1P4YaHb4bK_wAirOkN1Y8Izo9n_gjMCnE/edit#slide=id.p");
 }
